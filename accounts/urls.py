44945f65501268b9f27cfb8441372fd7ca39433e
AMAHMERCY <amahojiimercy@gmail.com>
2025-11-17 20:33:51 +0000

    this is the endpoint of the login

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import register_patient,login_user
 
 urlpatterns = [
-    path('register/', register_patient, name='register'),
-    path("login/", login_user, name="login"),
+    path('register/', register_patient, name='api_register'),
+    path("login/", login_user, name="api_login"),
 
 ]
